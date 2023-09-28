1650e12f7e9a494d8cc27cfd2ee08bf1707baedc
Rai Joia <megac154@gmail.com>
2023-09-28 16:03:21 -0300

    add memo in Item

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -2,10 +2,12 @@ import styles from './Item.module.scss';
 import { Prato } from 'types/Prato';
 import TagsPrato from 'components/TagsPrato';
 import { useNavigate } from 'react-router-dom';
+import { memo } from 'react';
 
-export default function Item(props: Prato) {
+function Item(props: Prato) {
   const { id, title, description, photo } = props;
   const navigate = useNavigate();
+  id === 1 && console.log(title);
   return (
     <div className={styles.item} onClick={() => navigate(`/prato/${id}`)}>
       <div className={styles.item__imagem}>
@@ -21,3 +23,6 @@ export default function Item(props: Prato) {
     </div>
   );
 }
+
+// para não renderizar sempre o react, renderiza apenas quando for totalmente destruído, muito usado em listas ou filtros
+export default memo(Item);
